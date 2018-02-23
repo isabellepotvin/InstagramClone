cedc9c7fdd06d82fbe3a2070e210ddd8d67e59ee
isabellepotvin <isabellepotvin@yahoo.ca>
2018-02-22 21:33:37 -0500

    End of part 11 - Account Settings Navigation

--- app/src/main/java/com/team06/InstagramClone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/team06/InstagramClone/Profile/AccountSettingsActivity.java
@@ -1,11 +1,18 @@
 package com.team06.InstagramClone.Profile;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.team06.InstagramClone.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by isabellepotvin on 2018-02-22.
@@ -15,10 +22,38 @@ public class AccountSettingsActivity extends AppCompatActivity{
 
     private static final String TAG = "AccountSettingsActivity";
 
+    private Context mContext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
         Log.d(TAG, "onCreate: started.");
+
+        mContext = AccountSettingsActivity.this;
+        setupSettingsList(); //displays settings list
+
+
+        //setup the back arrow for navigating back to "ProfileActivity"
+        ImageView backArrow = (ImageView) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigating back to 'ProfileActivity'");
+                finish();
+            }
+        });
+    }
+
+    private void setupSettingsList(){
+        Log.d(TAG,"setupSettingsList: initializing 'Account Settings' list.");
+        ListView listView = (ListView) findViewById(R.id.lvAccountSettings);
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/team06/InstagramClone/Profile/ProfileActivity.java
+++ app/src/main/java/com/team06/InstagramClone/Profile/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.team06.InstagramClone.R;
@@ -25,11 +26,17 @@ public class ProfileActivity extends AppCompatActivity{
     private static final int ACTIVITY_NUM = 4;
     private Context mContext = ProfileActivity.this;
 
+    private ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
+
+        //hides progress bar
+        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
 
         setupBottomNavigationView();
         setupToolbar();
