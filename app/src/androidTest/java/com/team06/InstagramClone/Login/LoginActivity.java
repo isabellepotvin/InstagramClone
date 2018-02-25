7a9105521003fc45c79cbd298dadf9a64e0e3fea
isabellepotvin <isabellepotvin@yahoo.ca>
2018-02-25 12:47:57 -0500

    End of part 19 - Get started with Firebase

---
+++ app/src/androidTest/java/com/team06/InstagramClone/Login/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.team06.InstagramClone.Login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.team06.InstagramClone.R;
+
+/**
+ * Created by isabellepotvin on 2018-02-25.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate: started.");
+        
+    }
+}
