2b095135046f9121648c19907c57d03e4f6c77c1
isabellepotvin <isabellepotvin@yahoo.ca>
2018-02-25 01:09:00 -0500

    End of part 14 - Universal Image Loader Config

--- app/src/main/java/com/team06/InstagramClone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/team06/InstagramClone/Profile/EditProfileFragment.java
@@ -2,11 +2,15 @@ package com.team06.InstagramClone.Profile;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.team06.InstagramClone.R;
+import com.team06.InstagramClone.Utils.UniversalImageLoader;
 
 /**
  * Created by isabellepotvin on 2018-02-24.
@@ -16,11 +20,30 @@ public class EditProfileFragment extends android.support.v4.app.Fragment {
 
     private static final String TAG = "EditProfileFragment";
 
+    private ImageView mProfilePhoto;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
 
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
+
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2016/08/ac-lloyd.jpg?itok=bb72IeLf";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 }
