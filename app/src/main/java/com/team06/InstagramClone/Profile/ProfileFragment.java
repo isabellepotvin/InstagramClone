6319fb7e7e34593a2b813a2c5bd7c0f2d291bbb4
isabellepotvin <isabellepotvin@yahoo.ca>
2018-03-10 12:34:43 -0500

    End of part 30 - Profile Fragment Setup

--- app/src/main/java/com/team06/InstagramClone/Profile/ProfileFragment.java
+++ app/src/main/java/com/team06/InstagramClone/Profile/ProfileFragment.java
@@ -1,13 +1,28 @@
 package com.team06.InstagramClone.Profile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.team06.InstagramClone.R;
+import com.team06.InstagramClone.Utils.BottomNavigationViewHelper;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by isabellepotvin on 2018-03-07.
@@ -17,11 +32,76 @@ public class ProfileFragment extends Fragment {
 
     private static final String TAG = "ProfileFragment";
 
+    private static final int ACTIVITY_NUM = 4;
+
+
+    private TextView mPosts, mFollowers, mFollowing, mDisplayName, mUsername, mWebsite, mDescription;
+    private ProgressBar mProgressBar;
+    private CircleImageView mProfilePhoto;
+    private GridView gridView;
+    private Toolbar toolbar;
+    private ImageView profileMenu;
+    BottomNavigationViewEx bottomNavigationView;
+
+    private Context mContext;
+    
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container,false);
 
+        mDisplayName = (TextView) view.findViewById(R.id.display_name);
+        mUsername = (TextView) view.findViewById(R.id.username);
+        mWebsite = (TextView) view.findViewById(R.id.website);
+        mDescription = (TextView) view.findViewById(R.id.description);
+        mProfilePhoto = (CircleImageView) view.findViewById(R.id.profile_photo);
+        mPosts = (TextView) view.findViewById(R.id.tvPosts);
+        mFollowers = (TextView) view.findViewById(R.id.tvFollowers);
+        mFollowing = (TextView) view.findViewById(R.id.tvFollowing);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.profileProgressBar);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        toolbar = (Toolbar) view.findViewById(R.id.profileToolbar);
+        profileMenu = (ImageView) view.findViewById(R.id.profileMenu);
+        bottomNavigationView = (BottomNavigationViewEx) view.findViewById(R.id.bottomNavViewBar);
+        mContext = getActivity();
+
+        Log.d(TAG, "onCreateView: started.");
+
+        setupBottomNavigationView();
+        setupToolbar();
+
         return view;
+    }
+
+    /**
+     * method that sets up toolbar
+     */
+    private void setupToolbar(){
+
+        ((ProfileActivity)getActivity()).setSupportActionBar(toolbar);
+        //Account settings button
+        profileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigation to account settings.");
+                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    // ***************************
+    // BottomNavigationView setup
+    // ***************************
+
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
     }
 }
