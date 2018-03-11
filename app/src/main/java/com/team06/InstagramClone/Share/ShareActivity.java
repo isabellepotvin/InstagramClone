5a179a126021664fb1937cd42681fd57dfd4c22c
isabellepotvin <isabellepotvin@yahoo.ca>
2018-03-11 12:52:30 -0400

    End of part 41 - Verifying Permissions for Sharing

--- app/src/main/java/com/team06/InstagramClone/Share/ShareActivity.java
+++ app/src/main/java/com/team06/InstagramClone/Share/ShareActivity.java
@@ -1,8 +1,10 @@
 package com.team06.InstagramClone.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,6 +13,7 @@ import android.view.MenuItem;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.team06.InstagramClone.R;
 import com.team06.InstagramClone.Utils.BottomNavigationViewHelper;
+import com.team06.InstagramClone.Utils.Permissions;
 
 /**
  * Created by isabellepotvin on 2018-02-20.
@@ -18,8 +21,13 @@ import com.team06.InstagramClone.Utils.BottomNavigationViewHelper;
 
 public class ShareActivity extends AppCompatActivity{
     private static final String TAG = "ShareActivity";
-    private static final int ACTIVITY_NUM = 2;
     private Context mContext = ShareActivity.this;
+
+    //constants
+    private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
+
+
 
 
     @Override
@@ -28,9 +36,69 @@ public class ShareActivity extends AppCompatActivity{
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: started.");
 
-        setupBottomNavigationView();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){ //check if the permissions are allowed
+
+        }else{ //if they haven't been verified
+            verifyPermissions(Permissions.PERMISSIONS); //verify permissions
+        }
+
+
+
+        //setupBottomNavigationView();
 
     }
+
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+
+    /**
+     * Check an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions){
+        Log.d(TAG, "checkPermissionsArray: checking persmissions array.");
+
+        for(int i = 0; i < permissions.length; i++){
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check a signle permission if it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permissions was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permissions was granted for: " + permission);
+            return true;
+        }
+    }
+
+
+
+
 
 
     // ***************************
---
+++ app/src/main/java/com/team06/InstagramClone/Utils/Permissions.java
@@ -0,0 +1,16 @@
+package com.team06.InstagramClone.Utils;
+
+import android.Manifest;
+
+/**
+ * Created by isabellepotvin on 2018-03-11.
+ */
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
