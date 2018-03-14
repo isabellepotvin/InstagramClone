62ce9ebd0a09da392f07b288a23c1a64dd1597cf
isabellepotvin <isabellepotvin@yahoo.ca>
2018-03-14 10:45:19 -0400

    End of part 45 - GalleryFragment Layout

--- app/src/main/java/com/team06/InstagramClone/Share/GalleryFragment.java
+++ app/src/main/java/com/team06/InstagramClone/Share/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.team06.InstagramClone.Share;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.team06.InstagramClone.R;
 
@@ -19,13 +25,47 @@ public class GalleryFragment extends Fragment {
 
     private static final String TAG = "GalleryFragment";
 
+    //widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner directorySpinner;
+
+    //vars
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
+        galleryImage = (ImageView) view.findViewById(R.id.galleryImageView);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+
         Log.d(TAG, "onCreateView: started.");
 
+        //close button
+        ImageView shareClose = (ImageView) view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: closing the gallery fragment.");
+                getActivity().finish();
+            }
+        });
+
+        //button that takes us to the screen to finalize the post
+        TextView nextScreen = (TextView) view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigating to the final share screen.");
+
+            }
+        });
 
         return view;
     }
