abfc83d4cf5c3afdbf9cdf62590e7c1c6a05bae2
isabellepotvin <isabellepotvin@yahoo.ca>
2018-03-13 21:48:26 -0400

    End of part 43 - Setup Gallery and Photo Fragments

---
+++ app/src/main/java/com/team06/InstagramClone/Share/GalleryFragment.java
@@ -0,0 +1,33 @@
+package com.team06.InstagramClone.Share;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team06.InstagramClone.R;
+
+
+/**
+ * Created by isabellepotvin on 2018-02-21.
+ */
+
+public class GalleryFragment extends Fragment {
+
+    private static final String TAG = "GalleryFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        Log.d(TAG, "onCreateView: started.");
+
+        return view;
+    }
+}
+
+
