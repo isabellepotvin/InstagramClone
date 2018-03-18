b944946d2a843174f07af5ea6204f9e35655fe85
isabellepotvin <isabellepotvin@yahoo.ca>
2018-03-18 00:08:12 -0400

    End of part 53 - Convert Bitmap to Byte Array

--- app/src/main/java/com/team06/InstagramClone/Utils/FirebaseMethods.java
+++ app/src/main/java/com/team06/InstagramClone/Utils/FirebaseMethods.java
@@ -1,6 +1,7 @@
 package com.team06.InstagramClone.Utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.team06.InstagramClone.Models.UserSettings;
 import com.team06.InstagramClone.R;
 
@@ -66,13 +68,22 @@ public class FirebaseMethods {
             StorageReference storageReference = mStorageReference
                     .child(filePaths.FIREBASE_IMAGE_STORAGE + "/" + user_id + "/photo" + (count + 1));
 
+            //convert image url to bitmap
+            Bitmap bm = ImageManager.getBitmap(imgUrl);
+            byte[] bytes = ImageManager.getBytesFromBitmap(bm, 100);
 
-        }else if(photoType.equals(mContext.getString(R.string.profile_photo))){
-            Log.d(TAG, "uploadNewPhoto: uploading new PROFILE photo.");
+            UploadTask uploadTask = null;
+            uploadTask = storageReference.putBytes(bytes);
+
 
         }
 
         //case 2: new profile photo
+        else if(photoType.equals(mContext.getString(R.string.profile_photo))){
+            Log.d(TAG, "uploadNewPhoto: uploading new PROFILE photo.");
+
+        }
+
     }
 
     public int getImageCount(DataSnapshot dataSnapshot){
